ef9a4465f327ff70fcba477f6190f5a72ee5c171
user <user@users-MacBook-Pro.local>
2018-02-16 11:58:54 -0800

    added pictures and uppercased the name and type

--- pokeApp/src/app/http.service.ts
+++ pokeApp/src/app/http.service.ts
@@ -6,9 +6,4 @@ export class HttpService {
 
   constructor(private _http: HttpClient) { }
 
-  getRandomPokemon() {
-    let randomNumber = Math.floor(Math.random() * 248 + 1);
-    let tempObservable = this._http.get(`http://pokeapi.co/api/v2/pokemon/${randomNumber}/`);
-    tempObservable.subscribe(data => console.log(data));
-  }
 }
